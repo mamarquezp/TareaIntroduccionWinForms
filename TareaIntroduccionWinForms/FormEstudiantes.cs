ecb95382d601ec5c3cb237b19912c8914f843216
MikeMarquez <mamarquezp@alumnos.uspg.edu.gt>
2025-08-16 01:02:07 -0600

    :hammer: ajuste en labelListado

--- TareaIntroduccionWinForms/FormEstudiantes.cs
+++ TareaIntroduccionWinForms/FormEstudiantes.cs
@@ -3,7 +3,7 @@ namespace TareaIntroduccionWinForms
     public partial class FormEstudiantes : Form
     {
         private List<string> listaEstudiantes = new List<string>();
-        private const string Encabezado = "Nombre\t\tCarnet\t\tCarrera\n";
+        private readonly string Encabezado = "Nombre".PadRight(25) + "Carnet".PadRight(15) + "Carrera\n";
         private const string Separador = "-------------------------------------------------------------\n";
 
         public FormEstudiantes()
@@ -39,7 +39,7 @@ namespace TareaIntroduccionWinForms
                 return;
             }
 
-            string nuevoEstudiante = $"{txtBoxNombre.Text}\t\t{txtBoxCarnet.Text}\t{cmbBoxCarrera.SelectedItem.ToString()}";
+            string nuevoEstudiante = txtBoxNombre.Text.PadRight(20) + txtBoxCarnet.Text.PadRight(15) + cmbBoxCarrera.SelectedItem.ToString();
 
             listaEstudiantes.Add(nuevoEstudiante);
 
